91c95e9a5f553966b311d82d41636f677d327b14
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:25:18 +0200

    xd

---
+++ HoroScope/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace HoroScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your star sign:");
+            string starsign = Console.ReadLine();
+
+            string output = GetLineFromFiledata(starsign);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Color not found");
+            }
+
+        }
+
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\samples";
+            string fileName = "personality.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            return dataFromFile;
+        }
+
+        private static string GetLineFromFiledata(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+
+
+
+            }
+            return result;
+        }
+    }
+}
---
+++ chuck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\opilane\samples";
+            string fileName = "chuck.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+            
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            Random rnd = new Random();
+            Console.WriteLine(dataFromFile[rnd.Next(dataFromFile.Length)]);
+
+        }
+    }
+}
